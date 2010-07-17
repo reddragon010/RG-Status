095c3fde50bbb899cd3bad8a62640b429061e921
Michael Riedmann <mriedmann@RRIs-MacMini.local>
2010-07-17 20:58:45 +0200

    initial commit

---
+++ rg_status.php
@@ -0,0 +1,29 @@
+<table id="realm_status">
+  <tr>
+    <td class="rs_head" colspan="3">
+      Server-Status:
+    </td>
+  </tr>
+  <tr>
+    <td>
+      Login
+      <br>
+      <iframe src="rg_status/log_stat.php"></iframe>
+    </td>
+    <td>
+      PvP
+      <br>
+      <iframe src="rg_status/pvp_stat.php"></iframe>
+    </td>
+    <td>
+      PvE
+      <br>
+      <iframe src="rg_status/pve_stat.php"></iframe>
+    </td>
+  </tr>
+  <tr>
+    <td colspan="3" style="visibility:hidden; display:none;">
+      <p> PHP-Script by <a href="http://www.aldabo.de"> aldabo.de </a> </p>
+    </td>
+  </tr>
+</table>
