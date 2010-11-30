3a67927ec90b83b6cb8393ad12d2f558b1897e0d
Michael Riedmann <mriedmann@RRIs-MacMini.local>
2010-11-30 15:05:47 +0100

    removed iframe-option (to much server traffic) / rewrote main parts to give a json-api with callback instead of an simple js-file

--- functions.include.php
+++ functions.include.php
@@ -1,16 +1,4 @@
-<?php
-//returns template as string
-function include_template($filename,$realms) {
-    if (is_file($filename)) {
-        ob_start();
-        include $filename;
-        $contents = ob_get_contents();
-        ob_end_clean();
-        return $contents;
-    }
-    return false;
-}     
-
+<?php    
 function currurl($file) {
 	if($_SERVER['SERVER_PORT']!=80){
 		return str_replace($file, '','http://'.$_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'].$_SERVER['PHP_SELF']);
--- rgs.php
+++ rgs.php
@@ -7,49 +7,33 @@ if(isset($_GET['t'])){
 	$type = false;
 }
 //select which doc should be served
-if($type=="js"){
-	$cachefile = 'cache.js';
-} else {
-	$cachefile = 'cache.html';
-}
+$cachefile = 'cache.js';
 //if cache is present and up-to-date serve it and quit
-if (file_exists($cachefile) && (time() - $config['cache_timeout'] < filemtime($cachefile))) {
+if ($config['cache'] && file_exists($cachefile) && (time() - $config['cache_timeout'] < filemtime($cachefile))) {
 	include($cachefile);
 	exit;
 }
+
 //if cache is expired or missing start output-buffer
 ob_start();
-?>
-<?php
+
 include('realmstatus.class.php');
 include('functions.include.php');
+//getting & setting callback
+if(isset($_GET['callback'])){
+	$callback = $_GET['callback'];
+} else {
+	$callback = 'rgs';
+}
 //load all realms defined in the config-file
 foreach($config['realms'] as $realmconfig){
 	$realms[] = new realm($realmconfig['name'],$realmconfig['ip'],$realmconfig['port'],$config['socket_timeout']);
 }
-//generate js if js is selected
-if($type=='js'){ ?>
-<?php 
-echo("document.write('");
-echo(str_replace(array("\r", "\r\n", "\n", "\t"), '', include_template("template.php",$realms))); 
-echo("');");
-?>
-<?php } else { //generate html if something else is selected ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-   "http://www.w3.org/TR/html4/loose.dtd">
-
-<html lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta http-equiv="refresh" content="<?php echo($config['cache_timeout']); ?>;<?php echo($_SERVER['PHP_SELF']); ?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo(currurl('rgs.php')); ?>rgs.css">  
-</head>
-<body>
-<?php include("template.php"); ?>  
-</body>
-</html>  
-<?php } ?>
-<?php
+//generate js
+echo($callback);
+echo("(");
+echo(json_encode($realms));
+echo(");");
 //write output to cache-file
 $fp = fopen($cachefile, 'w');
 fwrite($fp, ob_get_contents());
